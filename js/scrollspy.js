fd01e829cd3b608e00052ce48832592c3126eed8
Echo <32479267+exho-g@users.noreply.github.com>
2023-05-03 09:22:10 +0800

    Test callback functions

--- js/scrollspy.js
+++ js/scrollspy.js
@@ -5,6 +5,8 @@
     throttle: 100,
     scrollOffset: 200, // offset - 200 allows elements near bottom of page to scroll
     activeClass: 'active',
+    onEnter: null,
+    onExit: null,
     getActiveElement: function(id) {
       return 'a[href="#' + id + '"]';
     }
@@ -32,6 +34,8 @@
        * @prop {Number} [throttle=100] - Throttle of scroll handler
        * @prop {Number} [scrollOffset=200] - Offset for centering element when scrolled to
        * @prop {String} [activeClass='active'] - Class applied to active elements
+       * @prop {Function} onEnter - Callback function called when entering the next active section
+       * @prop {Function} onExit - Callback function called when exiting the current active section
        * @prop {Function} [getActiveElement] - Used to find active element
        */
       this.options = $.extend({}, ScrollSpy.defaults, options);
@@ -205,6 +209,14 @@
       });
 
       if (ScrollSpy._visibleElements[0]) {
+        // onEnter callback
+        if (typeof this.options.onEnter === 'function') {
+          console.log(this);
+          console.log($(this.options.getActiveElement(ScrollSpy._visibleElements[0].attr('id'))));
+          console.log(ScrollSpy._visibleElements);
+          this.options.onEnter.call(this, $(this.options.getActiveElement(ScrollSpy._visibleElements[0].attr('id'))));
+        }
+
         $(this.options.getActiveElement(ScrollSpy._visibleElements[0].attr('id'))).removeClass(
           this.options.activeClass
         );
@@ -216,6 +228,7 @@
         } else {
           ScrollSpy._visibleElements.push(this.$el);
         }
+
       } else {
         ScrollSpy._visibleElements.push(this.$el);
       }
@@ -231,6 +244,14 @@
       });
 
       if (ScrollSpy._visibleElements[0]) {
+        // onExit callback
+        if (typeof this.options.onExit === 'function') {
+          console.log(this);
+          console.log($(this.options.getActiveElement(ScrollSpy._visibleElements[0].attr('id'))));
+          console.log(ScrollSpy._visibleElements);
+          this.options.onExit.call(this, $(this.options.getActiveElement(ScrollSpy._visibleElements[0].attr('id'))));
+        }
+
         $(this.options.getActiveElement(ScrollSpy._visibleElements[0].attr('id'))).removeClass(
           this.options.activeClass
         );
